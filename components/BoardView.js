cf3252d2c81cab2a6fa4cb9fad7cf61086b5e772
kcrimi <kevincrimi@gmail.com>
2016-03-31 20:03:09 -0400

    add click opacity animations

--- components/BoardView.js
+++ components/BoardView.js
@@ -6,7 +6,10 @@ import React, {
   Text,
   View,
   TouchableOpacity,
+  Animated,
+  Easing,
 } from 'react-native';
+import autobind from 'autobind-decorator';
 var {width, height} = require('Dimensions').get('window');
 const GRID_SIZE = 4;
 const CELL_SIZE = Math.floor(width * .2);
@@ -16,6 +19,15 @@ const TILE_SIZE = CELL_SIZE - CELL_PADDING * 2;
 const LETTER_SIZE = Math.floor(TILE_SIZE * .85);
 
 class BoardView extends Component {
+
+	constructor(props) {
+		super(props);
+		var opacities = new Array(GRID_SIZE * GRID_SIZE);
+		for (var i = 0; i < opacities.length; i++){
+			opacities[i] = new Animated.Value(1);
+		}
+		this.state = {opacities: opacities};
+	}
 
 	render() {
 		return (
@@ -29,29 +41,35 @@ class BoardView extends Component {
 		var result = [];
 		for (var row = 0; row < GRID_SIZE; row++) {
 			for (var col = 0; col < GRID_SIZE; col++) {
-				var key = row * GRID_SIZE + col;
-				var letter = String.fromCharCode(65 + key);
-				var position = {
+				var id = row * GRID_SIZE + col;
+				var letter = String.fromCharCode(65 + id);
+				var style = {
 					left: col * CELL_SIZE + CELL_PADDING,
 					top: row * CELL_SIZE + CELL_PADDING,
+					opacity: this.state.opacities[id],
 				};
-				result.push(this.renderTile(key, position, letter));
+				result.push(this.renderTile(id, style, letter));
 			}
 		}
 		return result;
 	}
 
-	renderTile(id, position, letter){
+	renderTile(id, style, letter) {
 		return (
-			<View key={id} style={ [styles.tile, position] }
+			<Animated.View key={id} style={ [styles.tile, style] }
 				onStartShouldSetResponder={() => this.clickTile(id)}>
 				<Text style={ styles.letter }> { letter } </Text>
-			</View>
+			</Animated.View>
 		);
 	}
 
 	clickTile(id) {
-		console.log(id);
+		var opacity = this.state.opacities[id];
+		opacity.setValue(.5);
+		Animated.timing(opacity, {
+			toValue: 1,
+			duration: 250,
+		}).start();
 	}
 }
 
